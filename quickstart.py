9d9209e75a7c09066fcb86da1a4f05096087553f
meshnesh <tonnie.nesh@gmail.com>
2016-02-04 07:45:00 +0300

    quickstart in python

---
+++ quickstart.py
@@ -0,0 +1,50 @@
+# print('Hello world!')
+
+
+
+# a, b = 10, 1
+# if a < b:
+# 	print('a({}) is less than b({})'.format(a,b))
+# else:
+# 	print('a({}) is not less than b({})'.format(a,b))
+
+# print('manga' if a< b else 'Hello')
+
+
+# while loop
+# a, b = 0,1
+# while b<50:
+# 	print()
+# 	pass
+
+
+# class fibonacci
+# class Fibonacci(s):
+# 	"""docstring for Fibonacci"""
+# 	def __init__(self, a,b):
+# 		self.a = a
+# 		self.b = b
+
+# def series(self):
+# 	while (True):
+# 		yield(self.b)
+# 		self.a, self.b = self.b, self.a + self.b
+
+# f = Fibonacci(0,1)
+# for r in f.series():
+# 	if r > 100: break
+# 	print(r, end=' ')
+
+
+# class animalactions(object):
+# 	"""docstring for animalactions"""
+# 	def bark(self): return self._doAction('bark')
+# 		def fur(self): return self._doAction('fur')
+# 		def quack(self): return self._doAction('quack')
+# 		def feathers(self): return self._doAction('feathers')
+# 		def speak(self): return self._doAction('speak')
+
+		
+		
+
+
