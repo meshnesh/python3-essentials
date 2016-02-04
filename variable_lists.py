1af6b04ada76e95d1089ef3c63c8ab07a1a6d011
meshnesh <tonnie.nesh@gmail.com>
2016-02-04 07:45:56 +0300

    variable lists in java

---
+++ variable_lists.py
@@ -0,0 +1,25 @@
+def main():
+	
+	# s= (1,2,3,4,5,6,7)
+
+	# print(type(s), s)
+
+
+
+	# s= [1,2,3,4,5,6,7]
+	# s.append(8)
+
+	# print(type(s), s)
+
+
+	# s= 'antony'
+
+	# print(type(s), s[2:6])
+
+	x= 'antony'
+	for a in x:
+		print (a)
+
+
+
+if __name__ == "__main__":main()
