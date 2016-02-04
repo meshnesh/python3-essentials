2fc48b2752e8746ba7961ff35d78306e9219278c
meshnesh <tonnie.nesh@gmail.com>
2016-02-04 07:40:25 +0300

    various assignments in python

---
+++ assingments.py
@@ -0,0 +1,46 @@
+#printing out an array
+
+# def main():
+# 	a=[1,2,3,4,5]
+# 	print(a)
+
+# if __name__ == "__main__": main()
+
+#printing out a number
+
+# def main():
+# 	a= 1
+# 	print(a)
+
+# if __name__ == "__main__": main()
+
+#printing out a string
+
+# def main():
+# 	a="one"
+# 	print(a)
+
+# if __name__ == "__main__": main()
+
+#printing out data type and content
+# def main():
+# 	a="one"
+# 	print(type(a), a)
+
+# if __name__ == "__main__": main()
+
+#you can print out multiple values
+# def main():
+# 	a, b=10 , 15
+# 	print(a, b)
+
+# if __name__ == "__main__": main()
+
+
+
+#this is a toople
+def main():
+	a=(1,2,3,4,5)
+	print(a)
+
+if __name__ == "__main__": main()
