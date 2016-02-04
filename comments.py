dc1915d6f63e8df93b9a876619d4c917d5e96c06
meshnesh <tonnie.nesh@gmail.com>
2016-02-04 07:39:19 +0300

    comments in python

---
+++ comments.py
@@ -0,0 +1,10 @@
+#this is a single line comment
+
+"""
+class animalactions(object):
+	"""docstring for animalactions"""
+	def bark(self): return self._doAction('bark')
+		def fur(self): return self._doAction('fur')
+		def quack(self): return self._doAction('quack')
+		def feathers(self): return self._doAction('feathers')
+		def speak(self): return self._doAction('speak')""" #this is a multi-line comment
