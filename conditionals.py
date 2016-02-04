f8c8407c9237693c6d1f27ea3b874e08445e230f
meshnesh <tonnie.nesh@gmail.com>
2016-02-04 07:37:07 +0300

    code for conditionals in python

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+def main():
+	a, b = 10, 10 
+	if a < b:
+
+		print("b is bigger")
+
+	elif a > b:
+		print("a is the first born")
+	else:
+		print("b is the same age as a")
+
+if __name__ == "__main__": main()
