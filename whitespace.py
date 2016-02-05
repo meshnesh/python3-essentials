77fa7616a4d0094111e8ece022851bc313a8aee1
meshnesh <tonnie.nesh@gmail.com>
2016-02-05 14:46:21 +0300

    white spaces in python

---
+++ whitespace.py
@@ -0,0 +1,8 @@
+def main():
+	print('This is the syntax.py file')
+#indentation is the white space
+#it tells python  the print statement is part of the 'def' fuction. the indentatio must be the same
+if __name__ == "__main__":main()
+
+def main():print('This is the syntax.py file')
+#if it has one line of code it doesnt matter it will still run
