725f7637c8a55646d4ea705dd4682bca66222f5b
meshnesh <tonnie.nesh@gmail.com>
2016-02-05 14:45:08 +0300

    variable list

---
+++ variables_strings.py
@@ -0,0 +1,19 @@
+def main():
+	
+	# s= r'This is \n a string'
+	# print(s)
+
+	n = 42
+	s= '''\
+	today is thursday
+	the UX day
+	where we learn how to mingle with people
+	in profesional way of cause
+	lets see what we are gonna
+	learn.
+	'''
+	print(s)
+
+
+
+if __name__ == "__main__":main()
