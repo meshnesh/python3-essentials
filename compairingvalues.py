fa12c86ab82d363ebf1490c174412c1a85822ea1
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:47:06 +0300

    compairing values.py

---
+++ compairingvalues.py
@@ -0,0 +1,46 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> 5 < 6
+True
+>>> 6<5
+False
+>>> 6>=5
+True
+>>> 6>=7
+False
+>>> 5 == 6
+False
+>>> 5 == 5
+True
+>>> 5 != 6
+True
+>>> 5 != 0
+True
+>>> x,y = 5 , 6
+>>> y = 5
+>>> x,y = 5 , 6
+>>> y is 5
+False
+>>> x is 6
+False
+>>> id(y)
+4297538112
+>>> id(x)
+4297538080
+>>> y=5
+>>> x is y
+True
+>>> x, y = [5],[5]
+>>> id(x)
+4385174024
+>>> id(y)
+4298649416
+>>> x == y
+True
+>>> x is y
+False
+>>> 
