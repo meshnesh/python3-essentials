3a65bc76047df5045ac4af4ae6535ab6c6a463e8
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:51:17 +0300

    ifconditionals.py

---
+++ ifconditionals.py
@@ -0,0 +1,12 @@
+def main():
+	choices = dict(
+			one = 'first'
+			two = 'second'
+			three = 'third'
+			four = 'fourth'
+			five = 'fifth'
+		)
+	v = 'one'
+	print(choices[v])
+
+if __name__ == '__main__':main()
