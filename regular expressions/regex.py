4ef8b85b21a6dc85c31e05ab156c25c27c43aaf6
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:54:18 +0300

    regular expressions folder

---
+++ regular expressions/regex.py
@@ -0,0 +1,21 @@
+# import re
+#
+# def main():
+#     fh = open("raven.txt")
+#     for line in fh:
+#         if re.search('(Len|Neverm)ore', line):
+#             print(line)
+#
+# if __name__ == "__main__": main()
+
+
+import re
+
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+    	match = re.search('(Len|Neverm)ore', line)
+        if match:
+            print(match.group())
+
+if __name__ == "__main__": main()
