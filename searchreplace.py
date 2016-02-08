36badd946d2aae339f6c477069cddc1bd9cbbe8e
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:57:05 +0300

    searchplace.py

---
+++ searchreplace.py
@@ -0,0 +1,20 @@
+# import re
+
+# def main():
+#     fh = open("raven.txt")
+#     for line in fh:
+#         print (re.sub('(Len|Neverm)ore','###',line))
+
+# if __name__ == "__main__": main()
+
+
+import re
+
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+    	match = re.search('(Len|Neverm)ore', line)
+        if match:
+            print(line.replace(match.group(),'###'))
+
+if __name__ == "__main__": main()
