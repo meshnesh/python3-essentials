2d08d6766483766adef711273d1d14bb9ccbd516
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:52:40 +0300

    operator precedence.py

---
+++ operator-precedence.py
@@ -0,0 +1,14 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> 5 * 25 + 14 / 2
+132.0
+>>> 5 *(25 + 14) / 2
+97.5
+>>> 
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44)
+SyntaxError: invalid syntax
+>>> 
