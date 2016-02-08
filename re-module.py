ed459a7343635eb9d7e3eb97bbb34995cca3df93
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:55:23 +0300

    re-module.py

---
+++ re-module.py
@@ -0,0 +1,11 @@
+# regular expression are very powerful method of matching patterns in text
+# can be used for simple search an d replsce to complex data manupilation
+
+# they are implemented in python by the "re" module
+
+# they are called in the format
+
+import re
+
+pattern =re.compile(r'\d\d\d')
+	if re.search(regex, line): print(line)
