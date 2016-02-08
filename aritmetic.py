323568ad60f7fdf7ece9e8fb7a46dfbc247aef04
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:45:24 +0300

    arithmetic.py

---
+++ aritmetic.py
@@ -0,0 +1,61 @@
+
+# addition
+# num1 = 5
+# num2 = 10
+# ans = num1 + num2
+
+# print ans
+
+# division
+# num1 = 5
+# num2 = 10
+# ans = num1 / num2
+
+# print ans
+
+
+# multiplication
+# num1 = 5
+# num2 = 10
+# ans = num1 * num2
+
+# print ans
+
+
+# subtraction
+# num1 = 5
+# num2 = 10
+# ans = num1 - num2
+
+# print ans
+
+
+# addition
+# num = 10
+# num +=5
+# print num
+
+
+# subtraction
+# num = 10
+# num -=5
+# print num
+
+
+# multiplication
+# num = 10
+# num *=5
+# print num
+
+
+# division
+# num = 10
+# num //=5
+# print num
+
+
+# division
+# num = 10
+# num /=5
+# print num
+
