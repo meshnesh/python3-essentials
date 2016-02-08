394371f53a9cd4626b1465942d8b211b3d146cfe
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 07:47:45 +0300

    exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+# Exceptions are the key method of handling errors in Python
+
+# "try" something, then catch an exception with "except"
+# try:
+# 	fh = open('filename')
+# except IOError as e:
+# else:
+# 	for I in fh:print(I)
+
+
+# you may raise your own exceptions with "raise"
