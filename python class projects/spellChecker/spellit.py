fb0c468c6cc629aea35685794b55d8562816e9a0
meshnesh <tonnie.nesh@gmail.com>
2016-02-08 10:43:11 +0300

    spellchecker project

---
+++ python class projects/spellChecker/spellit.py
@@ -0,0 +1,18 @@
+def spellcheck(sentence):
+	words = open('spell.words').readlines()
+	words = list(map(lambda x: x.strip(),words))
+	res = sentence.split()
+	ret = []
+	for item in res:
+		if(item not in words):
+			ret.append(item)
+	return ret
+
+def  main():
+	sentence = input("please enter a sentence\n")
+	lst = []
+
+	lst = spellcheck(sentence)
+	print(lst)
+
+if __name__ == "__main__":main()
